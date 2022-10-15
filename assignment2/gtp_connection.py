89f591decb57022f35b7510a561f46a2a88b2377
Che-Ming Chung <cheming@ualberta.ca>
2022-10-15 01:53:07 -0600

    Working

--- assignment2/gtp_connection.py
+++ assignment2/gtp_connection.py
@@ -45,6 +45,7 @@ class GtpConnection:
         board:
             Represents the current board state.
         """
+        self.table = {}
         self._debug_mode: bool = debug_mode
         self.go_engine = go_engine
         self.board: GoBoard = board
@@ -376,6 +377,7 @@ class GtpConnection:
             
             
     def solve_cmd(self, args: List[str]) -> None:
+        self.code_from_board()
         if self.board.current_player == 1:
             current_color = 'b'
             opponent_color = 'w'
@@ -384,34 +386,73 @@ class GtpConnection:
             opponent_color = 'b'
         
         result = self.negamax()
+        #If current player is winner
+
         if result:
+            legal_moves = GoBoardUtil.generate_legal_moves(self.board, self.board.current_player)
+            for move in legal_moves:
+                self.play_move_eff(move)
+                if self.table.get(self.code_from_board()) == current_color:
+                    self.respond("{} {}".format(current_color, format_point(point_to_coord(move, self.board.size)).lower()))
+                    return
+
+                self.board.board[move] = EMPTY
+                self.board.current_player = opponent(self.board.current_player)
             self.respond(current_color)
         # Winner is opponent
         if result == False:
             self.respond(opponent_color)
+            return
 
         # Cannot solve within timelimit
         #self.respond("unknown")
     
     def negamax(self) -> bool:
+        code = self.code_from_board()
+        result = self.table.get(code)
+        if result != None:
+            if self.board.current_player == 1 and result == 'b' or self.board.current_player == 2 and result == 'w':
+                return True
+            else:
+                return False
         legal_moves = GoBoardUtil.generate_legal_moves(self.board, self.board.current_player)
         if len(legal_moves) == 0:
+            if self.board.current_player == 1:
+                self.table[code] = 'w' 
+            else: 
+                self.table[code] = 'b' 
             return False
         for move in legal_moves:
             #old = self.board.copy()
             self.play_move_eff(move)
             #self.board.play_move(move, self.board.current_player)
+            
             isWin = not self.negamax()
 
             # Undo the move
             self.board.board[move] = EMPTY
             self.board.current_player = opponent(self.board.current_player)
-            
-    
-            
+            #self.board = old
+
             if (isWin):
+                if self.board.current_player == 1:
+                    self.table[code] = 'b' 
+                else: 
+                    self.table[code] = 'w' 
                 return True
+
+
+        if self.board.current_player == 1:
+             self.table[code] = 'w' 
+        else: 
+            self.table[code] = 'b' 
         return False
+
+    def code_from_board(self):
+        
+        return int('1'+(''.join(map(str, self.board.board.tolist()))).replace("3",""))
+        
+        
 
     def play_move_eff(self, move):
         self.board.board[move] = self.board.current_player
