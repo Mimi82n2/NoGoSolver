a2a8c180c5ff7f5219994dce3dd9b1e6ae8a6b77
Che-Ming Chung <cheming@ualberta.ca>
2022-10-16 18:47:48 -0600

    Remove assert

--- assignment2/board.py
+++ assignment2/board.py
@@ -47,7 +47,7 @@ class GoBoard(object):
         """
         Creates a Go board of given size
         """
-        assert 2 <= size <= MAXSIZE
+        #assert 2 <= size <= MAXSIZE
         self.reset(size)
 
     def reset(self, size: int) -> None:
@@ -66,10 +66,10 @@ class GoBoard(object):
         
     def copy(self) -> 'GoBoard':
         b = GoBoard(self.size)
-        assert b.NS == self.NS
-        assert b.WE == self.WE
+        #assert b.NS == self.NS
+        #assert b.WE == self.WE
         b.current_player = self.current_player
-        assert b.maxpoint == self.maxpoint
+        #assert b.maxpoint == self.maxpoint
         b.board = np.copy(self.board)
         return b
 
@@ -111,6 +111,7 @@ class GoBoard(object):
         self.board[point] = color
         #neighbors = self._neighbors(point)
         neighbors = self.neighbors[point]
+        
         #check for capturing
         for nb in neighbors:
             if self.board[nb] == opp_color:
@@ -119,6 +120,7 @@ class GoBoard(object):
                 #undo capturing move
                     self.board[point] = EMPTY
                     return False
+
         #check for suicide
         block = self._block_of(point)
         if not self._has_liberty(block):  
@@ -142,8 +144,8 @@ class GoBoard(object):
         return where1d(self.board == EMPTY)
 
     def row_start(self, row: int) -> int:
-        assert row >= 1
-        assert row <= self.size
+        #assert row >= 1
+        #assert row <= self.size
         return row * self.NS + 1
         
         
@@ -205,7 +207,7 @@ class GoBoard(object):
         all the points in the block 
         """
         color: GO_COLOR = self.get_color(stone)
-        assert is_black_white(color)
+        #assert is_black_white(color)
         return self.connected_component(stone)
 
     def connected_component(self, point: GO_POINT) -> np.ndarray:
@@ -215,7 +217,7 @@ class GoBoard(object):
         marker = np.full(self.maxpoint, False, dtype=np.bool_)
         pointstack = [point]
         color: GO_COLOR = self.get_color(point)
-        assert is_black_white_empty(color)
+        #assert is_black_white_empty(color)
         marker[point] = True
         while pointstack:
             p = pointstack.pop()
@@ -244,7 +246,7 @@ class GoBoard(object):
         Returns whether move was legal
         """
         
-        assert is_black_white(color)
+        #assert is_black_white(color)
         
         if self.board[point] != EMPTY:
             return False
